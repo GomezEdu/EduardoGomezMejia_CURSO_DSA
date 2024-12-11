91f3cd9190741c80a0b9a3096d7c8751937bc9e6
Eduardo Gómez Mejía <230110727@itsoeh.edu.mx>
2024-12-10 23:15:28 -0600

    Stacks Test 3 _ HanoiGame

--- learning/Stacks/.ipynb_checkpoints/stack_sandbox-checkpoint.py
+++ learning/Stacks/.ipynb_checkpoints/stack_sandbox-checkpoint.py
@@ -1,8 +1,8 @@
 from Node import Node
 import logging
 
-class Stack():
-    def __init__(self, top_item = None, size = 0, limit = 1000):
+class Stack:
+    def __init__(self, top_item=None, limit=1000, size=0):
         self.top_item = top_item
         self.limit = limit
         self.size = size
@@ -15,26 +15,27 @@ class Stack():
 
     def peek(self):
         # Método para ver el elemento en la parte superior de la pila sin eliminarlo.
-        if (self.is_empty()):
-            raise AttributeError("La pila está vacía")
-        return self.top_item.get_value()
+        if (not self.is_empty()):
+            return self.top_item.get_value()
+        else:
+            logging.warning("La pila esta totalmente vacia!")
 
-    def push(self, value):
-         # Método para agregar un elemento a la pila.
+    def push(self, value): 
+        # Método para agregar un elemento a la pila.
         if (self.has_space()):
             item = Node(value)
             item.set_next_node(self.top_item)
             self.top_item = item
             self.size += 1
         else:
-            print("La pila esta llena ¡No queda espacio!")
+            logging.warning("La pila esta llena ¡No queda espacio!")
 
     def pop(self):
-        # Método para eliminar y devolver el elemento en la parte superior de la pila.
-        if(not self.is_empty()):
-            item_to_remove = self.top_item
+        # Método para eliminar y devolver el valor en la parte superior de la pila.
+        item_to_remove = self.top_item
+        if (not self.is_empty()):
             self.top_item = item_to_remove.get_next_node()
             self.size -= 1
             return item_to_remove.get_value()
         else:
-            raise AttributeError("La pila está vacía!")
+            logging.warning("La pila esta totalmente vacia!") # Muestra una advertencia si la pila está vacía.
